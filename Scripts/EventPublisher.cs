b787919a512e64c0a40054d436a6219458211241
DavisButler10 <45404502+DavisButler10@users.noreply.github.com>
2020-03-05 19:16:43 -0700

    Add files via upload

---
+++ Scripts/EventPublisher.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+public class EventPublisher : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown("s"))
+        {
+            EventBus.TriggerEvent("Shoot");
+        }
+
+        if (Input.GetKeyDown("l"))
+        {
+            EventBus.TriggerEvent("Launch");
+        }
+    }
+}
